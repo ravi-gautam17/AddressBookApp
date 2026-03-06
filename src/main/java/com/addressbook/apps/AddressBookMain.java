49836699f247c16aa70e39399d0c14618ffddfec
Ravi Gautam <180750145+ravi-gautam17@users.noreply.github.com>
2026-03-06 23:59:32 +0530

    [Ravi]: added Book Main program for UC3-AddContacts

--- src/main/java/com/addressbook/apps/AddressBookMain.java
+++ src/main/java/com/addressbook/apps/AddressBookMain.java
@@ -1,5 +1,4 @@
 package com.addressbook.apps;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -20,17 +19,40 @@ public class AddressBookMain {
     			arr[1],arr[2],arr[3],arr[4],
     			Integer.parseInt(arr[5]), arr[6], arr[7]));
     }
-    
-    public static void main(String[] args ) throws IOException{
-    	BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
-    	
-    	String line ;
-    	while((line=read.readLine())!=null) {
-    		add(line);
+    public static void update(String name ,String s) {
+    	String[] arr = s.split(":");
+    	if(arr.length!=8) {
+    		throw new IllegalArgumentException("Invalid Input");
     	}
+    	for(Contacts c: contacts) {
+    		if((name).equalsIgnoreCase(c.getFirstName()+" "+c.getLastName())) {
+    			c.setFirstName(arr[0]);
+    			c.setLastName(arr[1]);
+    			c.setAddress(arr[2]);
+    			c.setCity(arr[3]);
+    			c.setState(arr[4]);
+    			c.setZip(Integer.parseInt(arr[5]));
+    			c.setPhoneNo(arr[6]);
+    			c.setEmail(arr[7]);
+    			return;
+    		}
+    	}
+    	System.out.println("User Not Found ");
+    }
+    public static void main(String[] args ) throws IOException{
+      
+    	add("lucky:pal:berkhera:bhopal:MP:12345:83056144536:pallucky936@gmail.com");
+    	add("Himesh:kurmi:baisa:sagar:MP:462022:89564122121:himeshkurmi@gmail.com");
+    	add("nageshwar:patel:maiyar:katni:MP:11111:7845129654:nageshwar@gmail.com");
+    	for(Contacts c : contacts) {
+    		System.out.println(c.toString());
+    	}
+    	update("himesh kurmi","Himesh:kurmi:Anand Nager:Bhopal:MP:462022:89564122121:himeshkurmi@gmail.com ");
+    	System.out.println("\n");
     	for(Contacts c : contacts) {
     		System.out.println(c.toString());
     	}
     }
     
 }
+    
