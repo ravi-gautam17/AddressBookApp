6f39b8af61513e514a227bb43e53eb0224f6d978
Ravi Gautam <180750145+ravi-gautam17@users.noreply.github.com>
2026-03-07 13:02:41 +0530

    [Ravi]: modified AddressBook program in UC10-CountPeopleByCityOrState

--- src/main/java/com/addressbook/apps/AddressBook.java
+++ src/main/java/com/addressbook/apps/AddressBook.java
@@ -1,9 +1,12 @@
 package com.addressbook.apps;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 import com.addressbook.apps.model.Contacts;
 
@@ -182,5 +185,13 @@ public class AddressBook {
 	    	state = state.toLowerCase();
 	    	return mapByState.get(state);
 	    }
+	    
+	   public Map<String,Integer> countByCity(){
+		   return mapByCity.entrySet().stream().collect(Collectors.toMap(a->a.getKey(),a->a.getValue().size()));
+	   }
 	   
+	   public Map<String,Integer> countByState(){
+		   return mapByState.entrySet().stream().collect(Collectors.toMap(a->a.getKey(),a->a.getValue().size()));
+	   }
+	
 }
