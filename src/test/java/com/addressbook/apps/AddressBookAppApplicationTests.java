dbdd5a4e5244ca6e5c6cb5fdbe0a69689fb153b6
Ravi Gautam <180750145+ravi-gautam17@users.noreply.github.com>
2026-03-10 12:05:00 +0530

    [Ravi]: added test program for UC16-ConnectDatabase

--- src/test/java/com/addressbook/apps/AddressBookAppApplicationTests.java
+++ src/test/java/com/addressbook/apps/AddressBookAppApplicationTests.java
@@ -1,13 +1,29 @@
 package com.addressbook.apps;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.sql.SQLException;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.addressbook.database.SQLOperation;
 
 @SpringBootTest
 class AddressBookAppApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+	
+	@Test
+	public void getDetailTest() {
+		try {
+			assertEquals(1,SQLOperation.getAll("book2").size());
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 }
